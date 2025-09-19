382f8df8961a1ece19b89957319a2fdd7b9ef29e
zhangwenjin <zwjhb12@163.com>
2025-09-19 10:26:32 +0800

    feat: increase error handler for signup.

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -3,17 +3,17 @@
 import React from 'react'
 import { Stack, TextField, Button, Link } from '@mui/material'
 import NextLink from 'next/link'
-import { useFormState } from 'react-dom'
+import { useActionState } from 'react'
 import createUser from './create-user'
 export default function Signup() {
-  const [state, formAction] = useFormState(createUser, { error: "" })
+  const [state, formAction] = useActionState(createUser, { error: "" })
 
   return (
     <form action={formAction} className="w-full max-w-xs">
       <Stack spacing={2} >
-        <TextField name="email" label="Email" variant="outlined" type="email" />
+        <TextField name="email" label="Email" variant="outlined" type="email" helperText={state.error} error={!!state.error} />
 
-        <TextField name="email" label="Password" variant="outlined" type="pasasword" />
+        <TextField name="password" label="Password" variant="outlined" type="password" helperText={state.error} error={!!state.error} />
 
         <Button type="submit" variant="contained">Singup</Button>
 
