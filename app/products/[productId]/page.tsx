35a9efc1559214f19c5cca2d257065ebc9e045e9
zhangwenjin <zwjhb12@163.com>
2025-09-25 14:51:36 +0800

    chore: change single product ui.

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,9 +1,35 @@
+import { Grid, Stack, Typography } from "@mui/material";
+import getProduct from "./get-product";
+import { getProductImage } from "../product-image";
+import Image from "next/image";
+
 
 interface SinglePorductProps {
   params: { productId: string };
 }
 export default async function SingleProduct({ params }: SinglePorductProps) {
-  return <p>
-    SingleProduct {params.productId}
-  </p>
+  const { productId } = await params;
+  const product = await getProduct(+productId);
+
+  return (
+    <Grid container marginBottom={"2rem"} rowGap={3}>
+      {product.imageExists && (
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Image src={getProductImage(product.id)}
+            width={0} height={0}
+            className="w-full sm:w-3/4 h-auto"
+            sizes="100vw" alt="Picture of the product" />
+        </Grid>
+      )}
+
+      <Grid size={{ xs: 12, md: 6 }}>
+        <Stack gap={3}>
+          <Typography variant="h2">{product.name}</Typography>
+          <Typography>{product.description}</Typography>
+          <Typography variant="h4">${product.price}</Typography>
+        </Stack>
+      </Grid>
+    </Grid >
+
+  )
 }
--- app/products/product.tsx
+++ app/products/product.tsx
@@ -1,25 +1,33 @@
-import { Card, Stack, Typography } from "@mui/material";
+"use client"
+
+import { Card, CardActionArea, Stack, Typography } from "@mui/material";
 import { Product as IProduct } from "./interface/product.interface";
-import { API_URL } from "../common/constants/api";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { getProductImage } from "./product-image";
+
 
 interface ProductProps {
   product: IProduct
 }
 
 export default function Product({ product }: ProductProps) {
+  const router = useRouter();
+
   return (
-    <Card className="p-4">
-      <Stack gap={3}>
-        <Typography variant="h4">{product.name}</Typography>
-        {
-          product.imageExists && (
-            <Image src={`${API_URL}/images/products/${product.id}.jpg`} width="0" height="0" className="w-full h-auto" sizes="100vw" alt="Picture of the product" />
-          )
-        }
-        <Typography>{product.description}</Typography>
-        <Typography>${product.price}</Typography>
-      </Stack>
-    </Card>
+    <CardActionArea onClick={() => router.push(`/products/${product.id}`)}>
+      <Card className="p-4">
+        <Stack gap={3}>
+          <Typography variant="h4">{product.name}</Typography>
+          {
+            product.imageExists && (
+              <Image src={getProductImage(product.id)} width="0" height="0" className="w-full h-auto" sizes="100vw" alt="Picture of the product" />
+            )
+          }
+          <Typography>{product.description}</Typography>
+          <Typography>${product.price}</Typography>
+        </Stack>
+      </Card>
+    </CardActionArea>
   )
 }
